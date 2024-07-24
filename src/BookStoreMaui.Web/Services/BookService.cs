de06d41a8c00b7023758776c314e25ef8c5b359e
Дмитрий Горбунов <bous07@mail.ru>
2024-07-24 10:06:07 +0300

    - Author Books Page - Extract out Books Grid and Popular Books to Re-usable Components

--- src/BookStoreMaui.Web/Services/BookService.cs
+++ src/BookStoreMaui.Web/Services/BookService.cs
@@ -128,10 +128,11 @@ namespace BookStoreMaui.Web.Services
             var query = context.Books.Where(b => b.Author.Slug == authorSlug);
 
             var totalCount = await query.CountAsync();
-            var books = await query.Select(b => new BookListDto(b.Id, b.Title, b.Image, new AuthorDto(b.Author.Name, b.Author.Slug)))
+            var books = await query
                                    .OrderByDescending(b => b.Id)
                                    .Skip((pageNo - 1) * pageSize)
                                    .Take(pageSize)
+                                   .Select(b => new BookListDto(b.Id, b.Title, b.Image, new AuthorDto(b.Author.Name, b.Author.Slug)))
                                    .ToArrayAsync();
 
             return new PagedResult<BookListDto>(books, totalCount);
